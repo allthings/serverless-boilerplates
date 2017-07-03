f6ff3b831978620279e08c47706aff74130c98e6
Marco Lüthy <marco@luethy.net>
2017-07-03 17:21:36 +0200

    wip

--- graphql/src/handler.js
+++ graphql/src/handler.js
@@ -27,25 +27,8 @@ export const graphiqlHandler = graphiqlLambda({
 })
 
 /*
-
-samples: https://github.com/apollographql/launchpad
-some reasons why i like gql and would pick it over REST/CRUD:
-- avoid overfetching
-- tools like apollo-client have built in caching solutions
-- support for query subscriptions—no need to come up with or use some additional websocket data-exchange format
-- very enjoyable to develop
-- easy to set up in lambda, koa, express
-- automatic documentation via the introspection (especially if you provide field description in schema)
-  - yea yea yea but but swagger... yea.. it's not the same.
-- very enjoyable to develop against, e.g. in react, or even standalone
-- tools exist to easily query gql apis outside of a react app, server side
-- the introspection lets you generate an SDK. perfect for a developer platform.
-- the introspection lets you generate type definitions for use in code (e.g. TypeScript, or Golang)
-- if you enjoy writing SQL queries, GQL will also be enjoyable
-- quick api iteration, support for @deprecated fields
-- easy code separation/organisation
-- easily express and query complex data structures
-- with the introspection, it's possible to visualise a graphql api
-- if you're skeptical, please, give it 5 minutes. ( https://signalvnoise.com/posts/3124-give-it-five-minutes )
+✏️ Generate API code or type annotations based on a GraphQL schema and query documents
+https://github.com/apollographql/apollo-codegen
+https://dev-blog.apollodata.com/graphql-dx-d35bcf51c943
 
 */
